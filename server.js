166445cbaec6360dd518b96009e2321797f08d25
Nikola-Pet <pet.nikola97@gmail.com>
2023-01-03 18:55:59 +0100

    vercel

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const api = require('./api');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+const host = 'localhost';
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/data', (req, res) => {
+    const data = api.get();
+    res.send(data);
+  });
+  
+  app.get('/data/:id', (req, res) => {
+    const id = req.params.id;
+    const data = api.getById(id);
+    res.send(data);
+  });
+
+  app.post('/data', (req, res) => {
+    const item = req.body;
+    api.insert(item);
+    res.send(item);
+  });
+
+  app.delete('/data/:id', (req, res) => {
+    const id = req.params.id;
+    api.deleteById(id);
+    res.send(id);
+  });
+  
+  app.listen(port, () => console.log(`Example app listening at http://${host}:${port}`));
